4644a05695104735802e3667d46e12868e09ba3d
Neto test <aryamorozkova555@gmail.com>
2023-06-16 12:12:18 +0300

    CsvFileSource

--- src/test/java/org/example/HWCycles/services/HolidayServiceTest.java
+++ src/test/java/org/example/HWCycles/services/HolidayServiceTest.java
@@ -2,14 +2,16 @@ package org.example.HWCycles.services;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.CsvSource;
 
 public class HolidayServiceTest {
         @ParameterizedTest
-        @CsvSource({
-                "10000, 3000, 20000, 3",
-                "100000, 60000, 150000, 2"
-        })
+//        @CsvSource({
+//                "10000, 3000, 20000, 3",
+//                "100000, 60000, 150000, 2"
+//        })
+        @CsvFileSource(files="src/test/resources/Holiday.csv")
         public void shouldCalculateForLowIncome(int income, int expenses, int threshold, long expected) {
             HolidayService service = new HolidayService();
 
